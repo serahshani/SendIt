8cd745b70716e1ccfa1bf882f92892c63587cf5e
mark-s-bit <markchege03@gmail.com>
2024-11-20 11:01:59 +0300

    intial done

--- frontend/sendit-fronend/src/App.jsx
+++ frontend/sendit-fronend/src/App.jsx
@@ -1,17 +1,20 @@
-import {  Outlet } from 'react-router-dom';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar'; // Import Navbar
 
-// 
 export const App = () => {
-    return (
-        <>
-            <header><Navbar /></header>
-            <main style={{width: '100vw'}}> 
-                <Outlet />
-            </main>
-            
-        </>
-    );
+  return (
+    <>
+      {/* Navbar */}
+      <header>
+        <Navbar />
+      </header>
+      {/* Main Content */}
+      <main style={{ width: '100vw' }}>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default App;
