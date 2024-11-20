4c2834b0eaeeac519d359b18efa712010c504feb
mark-s-bit <markchege03@gmail.com>
2024-11-20 10:23:19 +0300

    intial app.jsx

---
+++ frontend/sendit-fronend/src/App.jsx
@@ -0,0 +1,17 @@
+import {  Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar'; // Import Navbar
+
+// 
+export const App = () => {
+    return (
+        <>
+            <header><Navbar /></header>
+            <main style={{width: '100vw'}}> 
+                <Outlet />
+            </main>
+            
+        </>
+    );
+};
+
+export default App;
